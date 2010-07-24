b4e3006a4b360e3db9d72e0afc3fea762b4a8bfe
Andy Mikhailenko <andy@neithere.net>
2010-07-24 20:05:33 +0600

    Added function "field_frequency" to dark.discovery; upgraded to the latest Docu API; added pretty-printing for some discovery functions. (As of 2010-07-09.)

--- dark/discovery.py
+++ dark/discovery.py
@@ -32,28 +32,58 @@ from docu import *
 from docu.validators import exists
 
 
-__all__ = ['suggest_structures', 'document_factory']
+__all__ = [
+    'field_frequency', 'print_field_frequency',
+    'suggest_structures', 'print_suggest_structures',
+    'document_factory'
+]
 
 
-class CatchAll(Document):
-    pass
 
-
-def suggest_structures(db):
+def suggest_structures(query, having=None):
     """
     Analyses all documents in given database and returns a list of unique
     structures found. The usefullness of the result depends on the database:
     for a highly irregular database a very large and almost useless list can be
     generated, while a more or less regular database can be easily inspected by
     this function.
+
+    :param having:
+        list of field names that must be present in each structure.
+
     """
-    # TODO: same but query-based
-    structures = []
-    for d in CatchAll.objects(db):
-        cols = sorted(d.keys())
-        if cols not in structures:
-            structures.append(cols)
-    return structures
+    structures = {}  #[]
+    for d in query:
+        cols = tuple(sorted(d.keys()))
+        if having and not all(k in cols for k in having):
+            continue
+        structures.setdefault(cols, 0)
+        structures[cols] += 1
+#        if cols in structures:
+#            structures.append(cols)
+    return sorted(structures.iteritems(), key=lambda x:x[1], reverse=True)
+
+def print_suggest_structures(*args, **kwargs):
+    results = suggest_structures(*args, **kwargs)
+    for structure, frequency in results:
+        print u'×{frequency:>5} ... {structure}'.format(
+            frequency=frequency, structure=', '.join(structure))
+
+def field_frequency(query, having=None, raw=False):
+    freqs = {}
+    for document in query:
+        data = document._saved_state.data if raw else document
+        if having and not all(k in data for k in having):
+            continue
+        for name in data.keys():
+            freqs.setdefault(name, 0)
+            freqs[name] += 1
+    return sorted(freqs.iteritems(), key=lambda x:x[1], reverse=True)
+
+def print_field_frequency(*args, **kwargs):
+    results = field_frequency(*args, **kwargs)
+    for name, frequency in results:
+        print u'×{frequency:>5} ... {name}'.format(**locals())
 
 def document_factory(structure):
     """
